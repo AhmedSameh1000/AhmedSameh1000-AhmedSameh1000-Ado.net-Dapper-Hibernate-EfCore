433d44b3f553dd5414d1c6bd435debefda5bb050
Ahmed Sameh <ahmeds141000@gmail.com>
2023-10-22 02:53:03 +0300

    End_Dapper_Ado.net_NhiberNate

---
+++ EfCore3/Configuration/WalletMapping.cs
@@ -0,0 +1,37 @@
+﻿using EfCore1.Model;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+
+namespace EfCore3.ConfigurationWallet
+{
+	public class WalletMapping:ClassMapping<Wallet>
+	{
+        public WalletMapping()
+        {
+            Id(c => c.Id, c =>
+            {
+                c.Generator(Generators.Identity);
+                c.Type(NHibernateUtil.Int32);
+                c.Column("Id");
+                c.UnsavedValue(0);
+            });
+
+            Property(c => c.Holder, c =>
+            {
+                c.Length(50);
+                c.Type(NHibernateUtil.AnsiString);
+                c.NotNullable(true);
+            });  
+            Property(c => c.Balance, c =>
+            {
+                c.Type(NHibernateUtil.Decimal);
+                c.NotNullable(true);
+            });
+            Table("Wallets");
+        }
+
+    }
+
+}
---
+++ EfCore3/Program.cs
@@ -0,0 +1,129 @@
+﻿using EfCore1.Model;
+using Microsoft.Extensions.Configuration;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Mapping.ByCode;
+using System.Configuration;
+using Configuration = NHibernate.Cfg.Configuration;
+
+namespace EfCore3
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			SendCash(2, 1, 50000);
+			AllWallets().Print();
+		}
+		public static ISession CreateSession()
+		{
+			var Config = new ConfigurationBuilder()
+				.AddJsonFile("AppSetting.json")
+				.Build();
+			var ConStr = Config.GetSection("ConStr").Value;
+			var Mapper = new ModelMapper();
+			Mapper.AddMappings(typeof(Wallet).Assembly.ExportedTypes);
+			HbmMapping DomapinMapping = Mapper.CompileMappingForAllExplicitlyAddedEntities();
+			var HbConfig = new Configuration();
+			HbConfig.DataBaseIntegration(c =>
+			{
+				c.Driver<MicrosoftDataSqlClientDriver>();
+				c.Dialect<MsSql2012Dialect>();
+				c.ConnectionString = ConStr;
+				//c.LogSqlInConsole = true;
+				//c.LogFormattedSql = true;
+			});
+			HbConfig.AddMapping(DomapinMapping);
+
+
+			var SessionFactory=HbConfig.BuildSessionFactory();
+			var Session=SessionFactory.OpenSession();
+			return Session;
+		}
+		public static IEnumerable<Wallet> AllWallets()
+		{
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+				var Wallets = Session.Query<Wallet>();
+				return Wallets.ToList();
+			}
+		}	
+		public static Wallet GetById(int id)
+		{
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+				var Wallet = Session.Query<Wallet>().FirstOrDefault(c=>c.Id==id);
+				return Wallet;
+			}
+		}
+		public static void Print<T>(this IEnumerable<T> Source)
+		{
+			foreach (var item in Source)
+			{
+				Console.WriteLine($"{item}");
+			}
+		}
+		public static void Insert(Wallet wallet)
+		{
+
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+			
+				Session.Save(wallet);
+				Transaction.Commit();
+			}
+		}
+	
+		public static void Update(Wallet wallet)
+		{
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+
+				Session.Update(wallet);
+				Transaction.Commit();
+			}
+		}
+		public static void Delete(int ID)
+		{
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+				var Walllet = GetById(ID);
+				if (Walllet is null)
+				{
+					return;
+				}
+				Session.Delete(Walllet);
+				Transaction.Commit();
+			}
+		}
+
+		public static void SendCash(int from,int to,int amount)
+		{
+			using (var Session = CreateSession())
+			{
+				using var Transaction = Session.BeginTransaction();
+			
+				var UserToSend=GetById(from);
+				var UserToRecive=GetById(to);
+				if (UserToSend.Balance < amount)
+				{
+					return;
+				}
+				UserToSend.Balance -= amount;
+				UserToRecive.Balance += amount;
+
+				Session.Update(UserToSend);
+				Session.Update(UserToRecive);
+				Transaction.Commit();
+			}
+		}
+	}
+}
